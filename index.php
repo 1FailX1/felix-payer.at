70af7f839789eea8f0078c5930e2e302d7b9eb35
Felix Payer <1felixpayer1@gmail.com>
2023-10-15 23:42:33 +0200

    Added Fade Effect
    On Desktop Viewport: Two texts are animatedly faded in whenever appearing on screen

--- index.php
+++ index.php
@@ -13,23 +13,29 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+    <script defer src="index.js"></script>
 </head>
 
 <body>
     <div class="wrapper">
         <div class="flex-introduction">
-            <div class="flex-introduction-item slight-smooth-zoom">
+            <div class="flex-introduction-item hidden">
                 <h1>Hi, <br>I'm Felix Payer</h1>
                 <p>I am Felix Payer, a Media Technician and Gaming Coach. I've worked with countless individuals, as well as
                     smaller businesses. <br><br> I love sharing my knowledge with other individuals in form of online
                     courses,
                     1:1 consultations and free video tutorials. With 5+ years of experience, I create highly satisfactory
-                    products.</p>
+                    products.
+                </p>
             </div>
-            <div class="flex-introduction-image" style="object-fit: cover; max-width: 300px; text-align: left">
+            <div class="flex-introduction-image">
                 <img src="images/felix.PNG" style="width: 100%; border-radius: 3%;"></img>
             </div>
         </div>
+
+
+
+
         <div class="flex-reviews">
             <div class="flex-reviews-left">
                 <div class="flex-reviews-left-item medium-smooth-zoom">
@@ -55,7 +61,7 @@
                     </div>
                 </div>
             </div>
-            <div class="flex-reviews-right">
+            <div class="flex-reviews-right hidden">
                 <h2>Trusted by numerous clients</h2>
                 <p style="color: green; font-size: 20px; font-weight: 500;">Patrick Vandl</p>
                 <p style="font-size: 18px; font-style: italic;">This was the best coaching session I've had so far, a
@@ -63,108 +69,112 @@
                 </p>
             </div>
         </div>
+
+
+
+
         <form method="post">
-        <div class="flex-contact">
-            <div class="flex-contact-left">
-                <h2>Contact me</h2>
-                <p>Fill out the form and I'll get back to you shortly!</p>
-            </div>
-            <div class="flex-contact-right">
-                <div class="flex-contact-form-emailname">
-                    <div class="flex-contact-form-emailname-item">
-                        <p>Name<br>
-                            <input type="text" name="Name"
-                                style="margin-top:5px; padding:10px; width:100%; border-radius:8px;background-color:rgba(0,0,0,0.07);color:#111827; border:none">
+            <div class="flex-contact">
+                <div class="flex-contact-left">
+                    <h2>Contact me</h2>
+                    <p>Fill out the form and I'll get back to you shortly!</p>
+                </div>
+                <div class="flex-contact-right">
+                    <div class="flex-contact-form-emailname">
+                        <div class="flex-contact-form-emailname-item">
+                            <p>Name<br>
+                                <input type="text" name="Name"
+                                    style="margin-top:5px; padding:10px; width:100%; border-radius:8px;background-color:rgba(0,0,0,0.07);color:#111827; border:none">
+                        </div>
+                        <div class="flex-contact-form-emailname-item">
+                            <p>E-Mail<br>
+                                <input type="text" name="Email"
+                                    style="margin-top:5px; padding:10px; width:100%; border-radius:8px;background-color:rgba(0,0,0,0.07);color:#111827; border:none">
+                        </div>
                     </div>
-                    <div class="flex-contact-form-emailname-item">
-                        <p>E-Mail<br>
-                            <input type="text" name="Email"
-                                style="margin-top:5px; padding:10px; width:100%; border-radius:8px;background-color:rgba(0,0,0,0.07);color:#111827; border:none">
+                    <div>
+                        <p>Message
+                            <textarea name="Message"
+                                style="margin-top:5px; padding:10px; width:100%; height: 200px; border-radius:8px;background-color:rgba(0,0,0,0.07);color:#111827; border:none"></textarea>
+                        </p>
+                    </div>
+                    <div style="text-align: right;">
+                        <input class="button" type="submit" value="Send"/>
+                    </div>
+                    <div>
+                        <?php
+                            if (isset($_POST['Email'])) {
+
+                                // EDIT THE FOLLOWING TWO LINES:
+                                $email_to = "business@felix-payer.at";
+                                $email_subject = "New form submissions";
+
+                                function problem($error)
+                                {
+                                    echo "We're sorry, but there were error(s) found with the form you submitted. ";
+                                    echo "These errors appear below.<br><br>";
+                                    echo $error . "<br><br>";
+                                    echo "Please go back and fix these errors.<br><br>";
+                                    die();
+                                }
+
+                                // validation expected data exists
+                                if (
+                                    !isset($_POST['Name']) ||
+                                    !isset($_POST['Email']) ||
+                                    !isset($_POST['Message'])
+                                ) {
+                                    problem('We\'re sorry, but there appears to be a problem with the form you submitted.');
+                                }
+
+                                $name = $_POST['Name']; // required
+                                $email = $_POST['Email']; // required
+                                $message = $_POST['Message']; // required
+
+                                $error_message = "";
+                                $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+                                if (!preg_match($email_exp, $email)) {
+                                    $error_message .= 'The Email address you entered does not appear to be valid.<br>';
+                                }
+
+                                $string_exp = "/^[A-Za-z .'-]+$/";
+
+                                if (!preg_match($string_exp, $name)) {
+                                    $error_message .= 'The Name you entered does not appear to be valid.<br>';
+                                }
+
+                                if (strlen($message) < 2) {
+                                    $error_message .= 'The Message you entered do not appear to be valid.<br>';
+                                }
+
+                                if (strlen($error_message) > 0) {
+                                    problem($error_message);
+                                }
+
+                                $email_message = "Form details below.\n\n";
+
+                                function clean_string($string)
+                                {
+                                    $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+                                    return str_replace($bad, "", $string);
+                                }
+
+                                $email_message .= "Name: " . clean_string($name) . "\n";
+                                $email_message .= "Email: " . clean_string($email) . "\n";
+                                $email_message .= "Message: " . clean_string($message) . "\n";
+
+                                // create email headers
+                                $headers = 'From: ' . $email . "\r\n" .
+                                    'Reply-To: ' . $email . "\r\n" .
+                                    'X-Mailer: PHP/' . phpversion();
+                                mail($email_to, $email_subject, $email_message, $headers);
+                                echo "Thanks for getting in touch. We'll get back to you soon.";
+                            }
+                        ?>
                     </div>
                 </div>
-                <div>
-                    <p>Message
-                        <textarea name="Message"
-                            style="margin-top:5px; padding:10px; width:100%; height: 200px; border-radius:8px;background-color:rgba(0,0,0,0.07);color:#111827; border:none"></textarea>
-                    </p>
-                </div>
-                <div style="text-align: right;">
-                    <input class="button" type="submit" value="Send"/>
-                </div>
-                <div>
-                    <?php
-                        if (isset($_POST['Email'])) {
-
-                            // EDIT THE FOLLOWING TWO LINES:
-                            $email_to = "business@felix-payer.at";
-                            $email_subject = "New form submissions";
-
-                            function problem($error)
-                            {
-                                echo "We're sorry, but there were error(s) found with the form you submitted. ";
-                                echo "These errors appear below.<br><br>";
-                                echo $error . "<br><br>";
-                                echo "Please go back and fix these errors.<br><br>";
-                                die();
-                            }
-
-                            // validation expected data exists
-                            if (
-                                !isset($_POST['Name']) ||
-                                !isset($_POST['Email']) ||
-                                !isset($_POST['Message'])
-                            ) {
-                                problem('We\'re sorry, but there appears to be a problem with the form you submitted.');
-                            }
-
-                            $name = $_POST['Name']; // required
-                            $email = $_POST['Email']; // required
-                            $message = $_POST['Message']; // required
-
-                            $error_message = "";
-                            $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-
-                            if (!preg_match($email_exp, $email)) {
-                                $error_message .= 'The Email address you entered does not appear to be valid.<br>';
-                            }
-
-                            $string_exp = "/^[A-Za-z .'-]+$/";
-
-                            if (!preg_match($string_exp, $name)) {
-                                $error_message .= 'The Name you entered does not appear to be valid.<br>';
-                            }
-
-                            if (strlen($message) < 2) {
-                                $error_message .= 'The Message you entered do not appear to be valid.<br>';
-                            }
-
-                            if (strlen($error_message) > 0) {
-                                problem($error_message);
-                            }
-
-                            $email_message = "Form details below.\n\n";
-
-                            function clean_string($string)
-                            {
-                                $bad = array("content-type", "bcc:", "to:", "cc:", "href");
-                                return str_replace($bad, "", $string);
-                            }
-
-                            $email_message .= "Name: " . clean_string($name) . "\n";
-                            $email_message .= "Email: " . clean_string($email) . "\n";
-                            $email_message .= "Message: " . clean_string($message) . "\n";
-
-                            // create email headers
-                            $headers = 'From: ' . $email . "\r\n" .
-                                'Reply-To: ' . $email . "\r\n" .
-                                'X-Mailer: PHP/' . phpversion();
-                            mail($email_to, $email_subject, $email_message, $headers);
-                            echo "Thanks for getting in touch. We'll get back to you soon.";
-                        }
-                    ?>
-                </div>
             </div>
-        </div>
         </form>
     </div>
 </body>
