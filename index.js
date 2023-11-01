0f14ce57ed6aed883eaf628b7cc8f3eeae5ebede
Felix Payer <1felixpayer1@gmail.com>
2023-11-01 11:10:09 +0100

    Redone entire site
    Homepage got new design and animations. New Images and Icons added. Redirect page to my YouTube video added. index.js now watches for many animations. TODO: Viewports

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const observer = new IntersectionObserver((entries) => {
         console.log(entry)
         if(entry.isIntersecting) {
             entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
         }
     });
 });
@@ -12,3 +10,63 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+// Bottom-To-Top
+
+const observer_bottom_to_top = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show-bottom-to-top');
+        }
+    });
+});
+
+
+const hiddenElements_bottom_to_top = document.querySelectorAll('.hidden-bottom-to-top');
+hiddenElements_bottom_to_top.forEach((el) => observer_bottom_to_top.observe(el));
+
+// Top-To-Bottom
+
+const observer_top_to_bottom = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show-top-to-bottom');
+        }
+    });
+});
+
+
+const hiddenElements_top_to_bottom = document.querySelectorAll('.hidden-top-to-bottom');
+hiddenElements_top_to_bottom.forEach((el) => observer_top_to_bottom.observe(el));
+
+// Left-To-Right
+
+const observer_left_to_right = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show-left-to-right');
+        }
+    });
+});
+
+
+const hiddenElements_left_to_right = document.querySelectorAll('.hidden-left-to-right');
+hiddenElements_left_to_right.forEach((el) => observer_left_to_right.observe(el));
+
+// Right-To-Left
+
+const observer_right_to_left = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show-right-to-left');
+        }
+    });
+});
+
+
+const hiddenElements_right_to_left = document.querySelectorAll('.hidden-right-to-left');
+hiddenElements_right_to_left.forEach((el) => observer_right_to_left.observe(el));
--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <!doctype html>
-<html>
+<html lang="de">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Ich bin Felix Payer: Website Designer, Content Creator, Gaming Coach und Programmierer. Sehen Sie sich meine Website an, um mehr über meine Services zu lernen!">
+    <meta name="keywords" content="Felix Payer, Payer, Webdevelopment, Content Creation, Gaming Coach">
+
     <title>Felix Payer</title>
     <link rel="stylesheet" type="text/css" href="CSS/index_mobile_viewport.css">
     <link rel="stylesheet" type="text/css" href="CSS/index_tablet_viewport.css">
@@ -18,66 +21,137 @@
 
 <body>
     <div class="wrapper">
-        <div class="flex-introduction">
-            <div class="flex-introduction-item hidden">
-                <h1>Hi, <br>I'm Felix Payer</h1>
-                <p>I am Felix Payer, a Media Technician and Gaming Coach. I've worked with countless individuals, as well as
-                    smaller businesses. <br><br> I love sharing my knowledge with other individuals in form of online
-                    courses,
-                    1:1 consultations and free video tutorials. With 5+ years of experience, I create highly satisfactory
-                    products.
-                </p>
-            </div>
-            <div class="flex-introduction-image">
-                <img src="images/felix.PNG" style="width: 100%; border-radius: 3%;"></img>
+        <div class="header">
+            <div class="header-subcontainer">
+                <a style="font-size: 40px; padding-right: 25px; padding-left: 10px; margin-right: auto" href="https://felix-payer.at/">felix-payer</a>
+                <a href="#services" style="padding: 25px">SERVICES</a>
+                <a href="#webdev" style="padding: 25px">WEB DEVELOPMENT</a>
+                <a href="#contentcreation" style="padding: 25px">CONTENT CREATION</a>
+                <a href="#coaching" style="padding: 25px">COACHING</a>
+                <a href="#contact" style="padding: 25px">KONTAKT</a>
             </div>
         </div>
 
 
-
-
-        <div class="flex-reviews">
-            <div class="flex-reviews-left">
-                <div class="flex-reviews-left-item medium-smooth-zoom">
-                    <a href="https://www.fiverr.com/failx_editz" style="pointer-events: auto; position:absolute; width:122px; height:290px"></a>
-                    <div class="center-children">
-                        <h1 class="font-resizable-70px">4.9</h1>
-                        <h2 style="color: #f0ad4e; margin-top: -15px">★★★★★</h2>
-                        <h4 style="margin-bottom: 0; padding-bottom: 0">40+ Ratings</h4>
-                        <h4 style="margin-top: 0; padding-top: 0">Fiverr Reviews</h4>
-                        <div class="flex-reviews-fiverr-image medium-smooth-zoom" style="object-fit: cover; max-width: 100px;">
-                            <img src="images/fiverr_icon.png" style="width: 100%;"></img>
-                        </div>
-                    </div>
+        <div class="flex-introduction-wrapper">
+            <div class="flex-introduction-innercontainer"> 
+                <div class="flex-introduction-item hidden">
+                    <h1>Ich bin Felix Payer</h1>
+                    <p>Ich bin Felix Payer, ein Web-Designer, Content Creator und Gaming Coach. Ich habe bereits mit unzähligen Einzelpersonen, als auch Unternehmen gearbeitet. <br><br> Ich lehre mein Wissen gerne mittels 1-on-1 Coachings und Video Tutorials. Mit über fünf Jahren intensiver Erfahrung habe ich viele zufriedene Kunden angesammelt.
+                    </p>
                 </div>
-                <div class="flex-reviews-left-item medium-smooth-zoom" style="margin-left: 70px">
-                    <a href="https://www.youtube.com/@1failx1"  style="pointer-events: auto; position:absolute; width:270px; height:290px"></a>
-                    <div class="center-children">
-                        <h1 class="font-resizable-70px">150.000+</h1>
-                        <h4>Views on YouTube</h4>
-                        <div class="flex-reviews-youtube-image medium-smooth-zoom" style="object-fit: cover; max-width: 100px">     
-                            <img src="images/youtube_icon.png" style="width: 100%;"></img>
-                        </div>
-                    </div>
+                <div class="flex-introduction-image">
+                    <img src="images/felix.webp" alt="Felix Payer Nahaufnahme"></img>
                 </div>
-            </div>
-            <div class="flex-reviews-right hidden">
-                <h2>Trusted by numerous clients</h2>
-                <p style="color: green; font-size: 20px; font-weight: 500;">Patrick Vandl</p>
-                <p style="font-size: 18px; font-style: italic;">This was the best coaching session I've had so far, a
-                    really fun learning experience!
-                </p>
             </div>
         </div>
 
+        <div class="service-container" id="services" style="text-align: center">
+                <h1>Services</h1>
+                <p class="big-text">Eine Liste meiner Services</p>
+                <hr class="horizontal-line-01">
+                <div class="service-sub-container">
+                    <a href="#webdev">
+                        <div class="service-sub-element hidden-bottom-to-top">
+                            <img src="images/webdev_01.webp" style="width: 100%; height: auto" alt="Man in Smoking">
+                            <h3>Web Development</h3>
+                        </div>
+                    </a>
+                    <a href="#contentcreation">
+                        <div class="service-sub-element hidden-bottom-to-top" style="transition-delay: 200ms;">
+                            <img src="images/contentcreation_01.webp" style="width: 100%; height: auto" alt="SEO symbol">
+                            <h3>Content Creation</h3>
+                        </div>
+                    </a>
+                    <a href="#coaching">
+                        <div class="service-sub-element hidden-bottom-to-top" style="transition-delay: 400ms;">
+                            <img src="images/coaching_01.webp" style="width: 100%; height: auto" alt="Computer with code">
+                            <h3>Gaming Coaching</h3>
+                        </div>
+                    </a> 
+                </div>
+        </div>
 
+        <div class="service-container" id="webdev">
+            <div class="hidden-top-to-bottom">
+                <h2>Web Development</h2>
+                <p class="big-text">Mit über einem <b class="green-text">halben Jahrzehnt</b> Erfahrung im Web Development kann ich ganz nach Ihren Wünschen eine Website für Ihre Firma oder Privatperson erstellen.</p>
+                <hr class="horizontal-line-01" align="left">
+            </div>
+            <div class="service-sub-container">
+                <div class="service-sub-element hidden-left-to-right">
+                    <img src="images/icons/man_in_smoking.png" alt="Man in Smoking">
+                    <h3>Seriöser Auftritt</h3>
+                    <p>Ein professionelles Design Ihrer Marke schafft einen positiven ersten Eindruck bei potenziellen Kunden und verbessert die Stärke und Seriosität Ihrer Markenidentität.</p>
+                </div>
+                <div class="service-sub-element hidden-left-to-right" style="transition-delay: 200ms;">
+                    <img src="images/icons/seo.png" alt="SEO symbol">
+                    <h3>SEO optimiert</h3>
+                    <p>Ich lege bei meinen Produkten großen Wert auf SEO (Search Engine Optimization), was für ein hohes Ranking ihres Web-Auftritts in Suchmaschinen sorgt.</p>
+                </div>
+                <div class="service-sub-element hidden-left-to-right" style="transition-delay: 400ms;">
+                    <img src="images/icons/computer_with_code.png" alt="Computer with code">
+                    <h3>Moderne Technologien</h3>
+                    <p>Meine Webseiten sind nicht mit Templating-Tools, wie Wordpress, sondern mit den besten CSS- und JS-Frameworks handgefertigt, um aus der Masse herauszustechen.</p>
+                </div> 
+            </div>
+        </div>
 
+        <div class="service-container" id="contentcreation" style="text-align: right">
+            <div class="hidden-top-to-bottom">
+                <h2>Content Creation</h2>
+                <p class="big-text">Wenn es um das Aufnehmen, bis hin zur Audio- und Videobearbeitung und folglich dem Publishing von <b class="green-text">hervorragenden Videos</b> geht, dann bin ich gerne Ihre Ansprechperson.</p>
+                <hr class="horizontal-line-01" align="right">
+            </div>
+            <div class="service-sub-container">
+                <div class="service-sub-element hidden-right-to-left" style="transition-delay: 400ms;">
+                    <img src="images/icons/convinced.png" alt="Convinced Icon">
+                    <h3>Überzeugende Produkte</h3>
+                    <p>Mein Content, den ich für Firmen, Privatpersonen und mich produziert habe, konnte bis dato nahezu eine Million Zuschauer erreichen.</p>
+                </div>
+                <div class="service-sub-element hidden-right-to-left" style="transition-delay: 200ms;">
+                    <img src="images/icons/video.png" alt="Video symbol">
+                    <h3>Shortform Content</h3>
+                    <p>Ich bin spezialisiert auf Shortform Content, wie Reels, Shorts und TikTok-Videos, da ich mich mit den Algorithmen dieser Plattformen ausgezeichnet auskenne.</p>
+                </div>
+                <div class="service-sub-element hidden-right-to-left">
+                    <img src="images/icons/marketing.png" alt="Marketing Symbol">
+                    <h3>Modernes Marketing</h3>
+                    <p>Heutzutage bewerben viele Unternehmen ihre Produkte, Podcasts und Kampanien erfolgreich mittels Shortform Content – so auch meine Partner, deren Videos ich produziere!</p>
+                </div> 
+            </div>
+        </div>
+
+        <div class="service-container" id="coaching" style="text-align: left">
+            <div class="hidden-top-to-bottom">
+                <h2>Gaming Coach</h2>
+                <p class="big-text">Seit 2022 bin ich <b class="green-text">erfolgreich</b> bei dem Freelancing-Dienst „Fiverr“ als Coach für das Videospiel „Rocket League“ tätig.</p>
+                <hr class="horizontal-line-01" align="left">
+            </div>
+            <div class="service-sub-container">
+                <div class="service-sub-element hidden-left-to-right">
+                    <img src="images/icons/coach.png" alt="Coach Icon">
+                    <h3>Etablierter Coach</h3>
+                    <p>Nahezu 100 zufriedenen Kunden untermauern meine Fähigkeit zu lehren. Durch Kunden aus der ganzen Welt durfte ich viele Ansichten kennenlernen.</p>
+                </div>
+                <div class="service-sub-element hidden-left-to-right" style="transition-delay: 200ms;">
+                    <img src="images/icons/Client.png" alt="Client symbol">
+                    <h3>Wiederkehrende Kunden</h3>
+                    <p>Meine Services haben eine hohe Anzahl an wiederkehrenden Kunden, welche durch rapide Resultate von meinem Coaching begeistert wurden.</p>
+                </div>
+                <div class="service-sub-element hidden-left-to-right" style="transition-delay: 400ms;">
+                    <img src="images/icons/rating.png" alt="Rating Star Symbol">
+                    <h3>Hervorragende Bewertung</h3>
+                    <p>Von etwa 60 Bewertungen erhielten meine Dienste ein überzeugendes durchschnittliches Rating von 4.9 Sternen.</p>
+                </div> 
+            </div>
+        </div>
 
         <form method="post">
-            <div class="flex-contact">
-                <div class="flex-contact-left">
-                    <h2>Contact me</h2>
-                    <p>Fill out the form and I'll get back to you shortly!</p>
+            <div id="contact" class="flex-contact">
+                <div class="flex-contact-left hidden-left-to-right">
+                    <h2>Kontaktieren Sie mich</h2>
+                    <p>Füllen Sie das Formular aus, und ich werde Ihnen zeitnah antworten.</p>
                 </div>
                 <div class="flex-contact-right">
                     <div class="flex-contact-form-emailname">
@@ -93,7 +167,7 @@
                         </div>
                     </div>
                     <div>
-                        <p>Message
+                        <p>Nachricht
                             <textarea name="Message"
                                 style="margin-top:5px; padding:10px; width:100%; height: 200px; border-radius:8px;background-color:rgba(0,0,0,0.07);color:#111827; border:none"></textarea>
                         </p>
@@ -176,6 +250,11 @@
                 </div>
             </div>
         </form>
+
+        <div class="footer">
+            <p>Website by Felix Payer 2023 ©</p>
+            <p>Icons from <a style="color: #fff;" target="_blank" href="https://icons8.com">Icons8</a></p>
+        </div>
     </div>
 </body>
 
